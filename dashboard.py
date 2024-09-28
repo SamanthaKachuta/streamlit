5831f609652f23adc3a5060564ec18b3854b6404
Samantha Kachuta <87725955+SamanthaKachuta@users.noreply.github.com>
2024-09-29 01:52:07 +0200

    added a treemap

--- dashboard.py
+++ dashboard.py
@@ -116,3 +116,10 @@ with st.expander("View Data of TimeSeries:"):
     st.write(linechart.T.style.background_gradient(cmap="Blues"))
     csv = linechart.to_csv(index=False).encode("utf-8")
     st.download_button('Download Data', data=csv, file_name="TimeSeries.csv", mime='text/csv')
+
+#Create a treemap based on Region, category, sub-Category
+st.subheader("Hierachichal of Sales using Treemap")
+fig3 = px.treemap(filtered_df, path = ["Region", "Category", "Sub-Category"], values = "Sales",hover_data = ["Sales"],
+                  color = "Sub-Category")
+fig3.update_layout(width = 800, height = 650)
+st.plotly_chart(fig3, use_container_width=True)
