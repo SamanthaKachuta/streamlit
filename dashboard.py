010a67e32947459920fc9a249431667583e69c4e
Samantha Kachuta <87725955+SamanthaKachuta@users.noreply.github.com>
2024-09-29 01:09:28 +0200

    Set page title

--- dashboard.py
+++ dashboard.py
@@ -7,4 +7,5 @@ warnings.filterwarnings('ignore')
 import matplotlib
 print(matplotlib.__version__)
 
-
+st.set_page_config(page_title="Superstore!!!", page_icon=":bar_chart", layout="wide")
+st.title(":bar_chart: Sam SuperStore")
