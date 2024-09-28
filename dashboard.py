42b118a03ace4de5c44c61118511985b776be347
Samantha Kachuta <87725955+SamanthaKachuta@users.noreply.github.com>
2024-09-29 01:45:16 +0200

    created filters for region, state and city

--- dashboard.py
+++ dashboard.py
@@ -36,3 +36,55 @@ with col2:
     date2 = pd.to_datetime(st.date_input("End Date", endDate))
 
 df = df[(df["Order Date"] >= date1) & (df["Order Date"] <= date2)].copy()
+
+#Create for Region
+st.sidebar.header("Choose your filter: ")
+region = st.sidebar.multiselect("Pick the Region", df["Region"].unique())
+if not region:
+    df2 = df.copy()
+else:
+    df2 = df[df["Region"].isin(region)]
+    
+#Create for state
+
+state = st.sidebar.multiselect("Pick the State", df2["State"].unique())
+if not state:
+    df3 = df2.copy()
+else:
+    df3 = df2[df["State"].isin(state)]
+
+#Create for city
+city = st.sidebar.multiselect("Pick the City", df3["City"].unique())
+
+#Filter the data based on Region, State and City
+
+if not region and not state and not city:
+    filtered_df = df
+elif not state and not city:
+    filtered_df = df[df["Region"].isin(region)]
+elif not region and not city:
+    filtered_df = df[df["State"].isin(state)]
+elif state and city:
+    filtered_df = df3[df["State"].isin(state) & df3["City"].isin(city)]
+elif region and city:
+    filtered_df = df3[df["Region"].isin(region) & df3["City"].isin(city)]
+elif region and city:
+    filtered_df = df3[df["Region"].isin(region) & df3["State"].isin(state)]
+elif city:
+    filtered_df = df3[df3["City"].isin(city)]
+else:
+    filtered_df = df3[df3["Region"].isin(region) & df3["State"].isin(state) & df3["City"].isin(city)]
+
+category_df = filtered_df.groupby(by = ["Category"], as_index = False)["Sales"].sum()
+
+with col1:
+    st.subheader("Category wise Sales")
+    fig = px.bar(category_df, x = "Category", y = "Sales", text = ['${:,.2f}'.format(x) for x in category_df["Sales"]],
+                 template= "seaborn")
+    st.plotly_chart(fig,use_container_width=True, height=200)
+
+with col2:
+    st.subheader("Region wise Sales")
+    fig = px.pie(filtered_df, values="Sales", names= "Region", hole= 0.5)
+    fig.update_traces(text=filtered_df["Region"],textposition = "outside")
+    st.plotly_chart(fig,use_container_width=True)
