d4bceab02317480141a1642b2e92c9ada5e6a856
Samantha Kachuta <87725955+SamanthaKachuta@users.noreply.github.com>
2024-09-29 01:10:55 +0200

    Uploaded csv file

--- dashboard.py
+++ dashboard.py
@@ -9,3 +9,13 @@ print(matplotlib.__version__)
 
 st.set_page_config(page_title="Superstore!!!", page_icon=":bar_chart", layout="wide")
 st.title(":bar_chart: Sam SuperStore")
+
+st.markdown('<style>div.block-container{padding-top:2rem}</style>',unsafe_allow_html=True)
+fl = st.file_uploader(":file_folder: Upload a file", type=(["csv","txt","xlsx","xls"]))
+if fl is not None:
+    filename = fl.name
+    st.write(filename)
+    df = pd.read_csv(filename, encoding= "ISO-8859-1")
+else:
+    os.chdir(r"C:\Users\ze9031311\Desktop\Streamlit")
+    df = pd.read_csv("Superstore.csv", encoding = "ISO-8859-1")
